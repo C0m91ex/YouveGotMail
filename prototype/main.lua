84506b33c2a57b5d3056d99e89ac6492648ad300
LeCodingOwl <nguyenjimmy627@gmail.com>
2025-01-17 12:27:41 -0800

    Set up basic layout

---
+++ prototype/main.lua
@@ -0,0 +1,14 @@
+
+-- loads everything once when runningthis file
+function love.load()
+
+end
+
+-- updates every frame
+-- do your math stuff here
+function love.update(dt)
+
+end
+
+-- similar to .update but this one is used to draw stuff onto the game screen
+function love.draw()
