bdefc57879df45afc66ca23dc7cb1a3a7de81c7d
LeCodingOwl <nguyenjimmy627@gmail.com>
2025-01-20 20:59:37 -0800

    Email can be thrown away

--- prototype/main.lua
+++ prototype/main.lua
@@ -6,13 +6,24 @@ local screen = {
 screen.width = screen.width / 2
 screen.height = screen.height / 2
 
+
 local spawnButton = {
     mode = "fill",
-    x = screen.width - 300,
-    y = screen.height,
+    x = screen.width,
+    y = screen.height + 200,
     width = 100,
     height = 50,
     color = {1, 1, 0} -- set color to yellow
+}
+
+
+local trashBin = {
+    mode = "fill",
+    x = screen.width - 380,
+    y = screen.height - 250,
+    width = 100,
+    height = 50,
+    color = {1, 0, 0} -- set color to red
 }
 
 local emails = {}
@@ -30,7 +41,11 @@ end
 
 -- loads everything once when runningthis file
 function love.load()
-    --spawnEmail("fill", screen.width + screen.width / 2, screen.height + screen.height / 2, 50, 50, {1, 1, 1})
+    temptY = 250
+    for x = 1, 3, 1 do
+        spawnEmail("fill", screen.width - 250, screen.height - temptY, 500, 50, {1, 1, 1})
+        temptY = temptY - 70
+    end
 end
 
 -- updates every frame
@@ -39,10 +54,17 @@ function love.update(dt)
     -- check if left mouse button is down
     if love.mouse.isDown(1) then
         -- just like in the draw function, iterates each email in emails table, checking that specific email's x and y with mouse's x and y position
+        -- this is for making the box draggable. it's not perfect but it gets the job done
         for _, email in ipairs(emails) do
-            if love.mouse.getX() > email.x and love.mouse.getX() < email.x + email.height and love.mouse.getY() > email.y and love.mouse.getY() < email.y + email.height then
+            if love.mouse.getX() > email.x and love.mouse.getX() < email.x + email.width and love.mouse.getY() > email.y and love.mouse.getY() < email.y + email.height then
                 email.x = love.mouse.getX() - (email.width / 2)
                 email.y = love.mouse.getY() - (email.height / 2)
+            end
+
+            -- Checks to see if a email is above the trash bin, if so, delete it
+            if email.x > trashBin.x and email.x < trashBin.x + trashBin.width and email.y > trashBin.y and email.y < trashBin.y + trashBin.height then
+                print("throw away email")
+                table.remove(emails, _)
             end
         end
     end
@@ -50,6 +72,10 @@ end
 
 -- similar to .update but this one is used to draw stuff onto the game screen
 function love.draw()
+    -- drow trash bin
+    love.graphics.setColor(trashBin.color)
+    love.graphics.rectangle(trashBin.mode, trashBin.x, trashBin.y, trashBin.width, trashBin.height)
+
     -- draw spawned email
     -- iterates through the emails table 
     -- ipairs: lua function used to iterate in a numerical order for talbes with sequential interger keys
@@ -61,14 +87,16 @@ function love.draw()
     -- draw spawn button
     love.graphics.setColor(spawnButton.color)
     love.graphics.rectangle(spawnButton.mode, spawnButton.x, spawnButton.y, spawnButton.width, spawnButton.height)
+
 end
+
 
 function love.mousepressed(x, y, button, istouch)
     -- checks if the left mouse button is pressed
     if button == 1 then
         -- checks if the mouse is on the yellow spawn button
         if x > spawnButton.x and x < spawnButton.x + spawnButton.width and y > spawnButton.y and y < spawnButton.y + spawnButton.height then
-            spawnEmail("fill", screen.width + 300, screen.height, 50, 50, {1, 1, 1})
+            table.remove(emails, email)
         end
     end
 end
